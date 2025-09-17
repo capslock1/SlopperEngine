af31b2b20052027b400e6fe04d141bf9e8b730c8
Caps Lock <c.storm@hotmail.nl>
2025-09-17 21:36:14 +0200

    Add CPU-side RGBA8Texture

---
+++ SlopperEngine/Graphics/CPUResources/RGBA8Texture.cs
@@ -0,0 +1,104 @@
+using OpenTK.Graphics.OpenGL4;
+using OpenTK.Mathematics;
+using SlopperEngine.Graphics.GPUResources.Textures;
+using StbImageSharp;
+
+namespace SlopperEngine.Graphics.CPUResources;
+
+/// <summary>
+/// A texture on CPU's memory, with 8 bit RGBA color.
+/// </summary>
+public class RGBA8Texture
+{
+    /// <summary>
+    /// The texture on VRAM.
+    /// </summary>
+    public Texture2D? GPUTexture { get; private set; }
+
+    /// <summary>
+    /// The width in pixels.
+    /// </summary>
+    public readonly int Width;
+
+    /// <summary>
+    /// The height in pixels.
+    /// </summary>
+    public readonly int Height;
+
+    readonly byte[] _pixels;
+    bool _gpuUpToDate;
+
+    public RGBA8Texture(int width, int height) : this(width, height, new byte[width * height * 4]){}
+
+    RGBA8Texture(int width, int height, byte[] pixels)
+    {
+        _pixels = pixels;
+        Width = width;
+        Height = height;
+
+        if (pixels.Length != width * height * 4)
+            Console.WriteLine($"RGBA8Texture received {width}*{height} as dimensions, but only received enough bytes for {pixels.Length / 4} pixels.");
+    }
+
+    /// <summary>
+    /// Loads a new RGBA8Texture from the disk.
+    /// </summary>
+    /// <param name="filepath">The full filepath to the texture.</param>
+    /// <returns>A new RGBA8Texture. This is not cached.</returns>
+    public static RGBA8Texture Load(string filepath)
+    {
+        StbImage.stbi_set_flip_vertically_on_load(1);
+
+        ImageResult image = ImageResult.FromStream(File.OpenRead(filepath), ColorComponents.RedGreenBlueAlpha);
+        var res = new RGBA8Texture(image.Width, image.Height, image.Data);
+
+        return res;
+    }
+
+    /// <summary>
+    /// Sets the GPUTexture to be up to date to the CPU side texture.
+    /// </summary>
+    public void UpdateGPU()
+    {
+        if (_gpuUpToDate)
+            return;
+
+        _gpuUpToDate = true;
+
+        if (GPUTexture == null)
+        {
+            GPUTexture = Texture2D.Create(
+                Width,
+                Height,
+                SizedInternalFormat.Rgba8,
+                PixelFormat.Rgba,
+                _pixels);
+            return;
+        }
+
+        GL.TextureSubImage2D(GPUTexture.Handle, 0, 0, 0, Width, Height, PixelFormat.Rgba, PixelType.UnsignedByte, _pixels);
+    }
+
+    /// <summary>
+    /// Tries to set a certain pixel to a certain color.
+    /// </summary>
+    /// <param name="position">The pixel to change.</param>
+    /// <param name="color">The color to set the color as.</param>
+    public bool TrySetPixel(Vector2i position, Color4 color)
+    {
+        if ((uint)position.X > Width) return false;
+        if ((uint)position.Y > Height) return false;
+
+        _gpuUpToDate = false;
+        var col = (Vector4)color;
+        col *= 255;
+        Vector4.Clamp(col, new(0), new(255));
+        int pos = (position.X + Width * position.Y) * 4;
+        _pixels[pos] = (byte)col.X;
+        _pixels[pos+1] = (byte)col.Y;
+        _pixels[pos+2] = (byte)col.Z;
+        _pixels[pos+3] = (byte)col.W;
+
+        return true;
+    }
+}
